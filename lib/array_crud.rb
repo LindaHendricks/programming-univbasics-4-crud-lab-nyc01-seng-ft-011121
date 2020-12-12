586f54d402fa677c579a39914d1bd1df63092d6f
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-12 18:08:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,31 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+ ["Linda", "Hugo", "Charlay", "Catto"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = ["I", "am", "really", "learning"]
+    element = "wow"
+    array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["I", "am", "really", "learning", "arrays!"]
+    array_minus_one_item = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array_start_without_one_item = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
